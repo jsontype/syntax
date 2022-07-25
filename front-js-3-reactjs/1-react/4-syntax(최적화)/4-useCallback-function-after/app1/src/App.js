181e5568c708132f2d78042449e606a6b1a967b3
jsontype <jsontyper@gmail.com>
2022-07-26 01:00:54 +0900

    tuning

--- front-js-3-reactjs/1-react/4-syntax(최적화)/4-useCallback-function-after/app1/src/App.js
+++ front-js-3-reactjs/1-react/4-syntax(최적화)/4-useCallback-function-after/app1/src/App.js
@@ -9,6 +9,9 @@ export default function App() {
   console.log("render App")
   const [count, setCount] = useState(0)
 
+  // Before: App이 업데이트 될 때마다 Child에도 함수가 걸려있어 재렌더링 된다. 때문에 App을 업데이트할 때에도 Child 컴포넌트가 재렌더링된다.
+  // const handleClick = () => { console.log("click") }, []
+
   // After: 아래처럼 함수를 유즈콜백으로 메모화시키면, 신, 구 handleClick는 동일한 값을 갖게 되어, Child 컴포넌트는 재렌더링되지 않는다.
   const handleClick = useCallback(() => { console.log("click") }, [])
 
--- front-js-3-reactjs/1-react/4-syntax(최적화)/8-useMemo-cost/app1/src/App.js
+++ front-js-3-reactjs/1-react/4-syntax(최적화)/8-useMemo-cost/app1/src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   const [count1, setCount1] = useState(0)
   const [count2, setCount2] = useState(0)
 
+  // 주석토글 1->2는 아래 함수를 쓰지 않고, 주석토글 3->4는 아래 함수를 쓴다. 하나씩 열어보면서 알아보자.
   const double = count => {
     let i = 0
     while (i < 3000000000) i++
@@ -14,7 +15,7 @@ export default function App() {
 
 
 
-  // <주석토글 1-1 Start>
+  // <주석토글 1> : "재렌더링 비용 <= useMemo 실행비용"이므로 1 -> 2로 가는 것이 의미가 없다.
   const Counter = (count2) => {
     console.log("render Counter")
     const doubledCount = count2 * 2
@@ -22,9 +23,8 @@ export default function App() {
       `Counter: ${count2}, ${doubledCount}`
     )
   }
-  // <주석토글 1-1 End>
 
-  // <주석토글1-2 Start> : "재렌더링 비용 <= useMemo 실행비용"이므로 하는 것이 의미가 없다.
+  // <주석토글 2> : "재렌더링 비용 <= useMemo 실행비용"이므로 1 -> 2로 가는 것이 의미가 없다.
   // const Counter = useMemo(() => {
   //   console.log("render Counter")
   //   const doubledCount = count2 * 2
@@ -32,12 +32,8 @@ export default function App() {
   //     <p>Counter: {count2}, {doubledCount}</p>
   //   )
   // }, [count2])
-  // <주석토글1-2 End>
   
-
-
-
-  // <주석토글 2-1 Start>
+  // <주석토글 3> : "재렌더링 비용 > useMemo 실행비용"이므로 분명히 재렌더링 비용이 높기 때문에 3 -> 4로 가서 useMemo를 사용하면 큰 효과를 얻을 수 있다.
   // const Counter = (count2) => {
   //   console.log("render Counter")
   //   const doubledCount = double(count2)
@@ -45,9 +41,8 @@ export default function App() {
   //     `Counter: ${count2}, ${doubledCount}`
   //   )
   // }
-  // <주석토글 2-1 End>
 
-  // <주석토글2-2 Start> : "재렌더링 비용 > useMemo 실행비용"이므로 분명히 재렌더링 비용이 높기 때문에 useMemo를 사용하면 큰 효과를 얻을 수 있다.
+  // <주석토글 4> : "재렌더링 비용 > useMemo 실행비용"이므로 분명히 재렌더링 비용이 높기 때문에 3 -> 4로 가서 useMemo를 사용하면 큰 효과를 얻을 수 있다.
   // const Counter = useMemo(() => {
   //   console.log("render Counter")
   //   const doubledCount = double(count2) // 이것 때문에 하는 게 더 낫다.
@@ -55,18 +50,17 @@ export default function App() {
   //     <p>Counter: {count2}, {doubledCount}</p>
   //   )
   // }, [count2])
-  // <주석토글2-2 End>
 
-  
+
 
   return (
     <>
       <p>Counter: {count1}</p>
       <button onClick={() => setCount1(count1 + 1)}>Increment count1</button>
 
-      {/* 주석토글 1-1, 2-1 */}
+      {/* 주석토글 1, 3의 경우 */}
       <p>{Counter(count2)}</p>
-      {/* 주석토글 1-2, 2-2 */}
+      {/* 주석토글 2, 4의 경우 */}
       {/* {Counter} */}
       <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
     </>
