3439971dcdb7fa55fee46cb00f419804cb2991d2
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2022-07-29 02:06:58 +0900

    react scss

---
+++ front-js-3-reactjs/2-nextjs/1-syntax/1-sass/app1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Button from './components/Button'
+
+function App () {
+  return (
+    <div className="App">
+      <div className="buttons">
+        <Button>BUTTON</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
