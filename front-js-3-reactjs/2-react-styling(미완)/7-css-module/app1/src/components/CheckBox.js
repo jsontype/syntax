b2b4448c31c81bdad5f332a1d9c7df7b69d6458c
jsontype <jsontyper@gmail.com>
2022-08-19 02:08:38 +0900

    scss

---
+++ front-js-3-reactjs/2-react-styling(미완)/7-css-module/app1/src/components/CheckBox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './CheckBox.module.css'
+
+function CheckBox({ children, checked, ...rest }) {
+    return (
+        <div className={styles.checkbox}>
+            <label>
+                <input type="checkbox" checked={checked} {...rest} />
+                <div>{checked ? '체크됨' : '체크 안됨'}</div>
+            </label>
+            <span>{children}</span>
+        </div>
+    )
+}
+
+export default CheckBox
