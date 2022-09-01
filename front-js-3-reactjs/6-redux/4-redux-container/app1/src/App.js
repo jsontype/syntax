8c06dc4f52333ab2c7c0815b3df2f05c8cd3e8e7
jsontype <jsontyper@gmail.com>
2022-09-01 23:21:25 +0900

    redux container

---
+++ front-js-3-reactjs/6-redux/4-redux-container/app1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// 컨테이너 컴포넌트를 렌더링
+import CounterContainer from './components/CounterContainer'
+
+function App() {
+  return (
+    <div>
+      <CounterContainer />
+    </div>
+  )
+}
+
+export default App
