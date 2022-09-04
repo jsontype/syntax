6dd19f21b574687ac55f4187fb8147c7a4e69ec0
jsontype <jsontyper@gmail.com>
2022-09-04 19:10:46 +0900

    redux container optimization

--- front-js-3-reactjs/6-state-management(17)/2-react-redux(hooks-useSelector-useDispatch)/7-redux-container-optimization/app1/src/components/Counter/CounterContainer.js
+++ front-js-3-reactjs/6-state-management(17)/2-react-redux(hooks-useSelector-useDispatch)/7-redux-container-optimization/app1/src/components/Counter/CounterContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch, shallowEqual } from 'react-redux'
 import Counter from './Counter'
 import { increase, decrease, setDiff } from '../../modules/counter'
 
@@ -7,14 +7,17 @@ import { increase, decrease, setDiff } from '../../modules/counter'
 // 컨테이너 컴포넌트란, 프레젠테이셔널 컴포넌트가 Redux store의 state를 조회하거나 action을 dispatch 할 수 있게 해주는 컴포넌트이다.
 // HTML 태그들을 사용하지 않고 다른 프리젠테이셔널 컴포넌트들을 불러와서 사용한다.
 
-function CounterContainer() {
+  function CounterContainer() {
     // 1. state 조회
     // Redux store의 state를 조회할 수 있게, useSelector Hook을 걸어준다.
-    // 이렇게 하면, "number, diff"의 state의 값은 store.getState() 함수를 호출한 결과값과 같아진다.
-    const { number, diff } = useSelector(state => ({
-        number: state.counter.number,
-        diff: state.counter.diff
-    }))
+    // 이렇게 하면, "number, diff"의 state의 값은 store.getState() 함수를 호출한 결과값과 같아진다.    
+    const { number, diff } = useSelector(
+        state => ({
+            number: state.counter.number,
+            diff: state.counter.diff
+        }),
+        shallowEqual
+    )
 
     // 2. action 디스패치 
     // useDispatch Hook을 걸어서, store의 dispatch를 함수에다가도 사용 할 수 있게 해준다.
