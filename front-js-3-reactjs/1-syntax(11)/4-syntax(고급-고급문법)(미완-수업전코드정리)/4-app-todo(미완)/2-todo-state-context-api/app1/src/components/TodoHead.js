c6dba7d2182c91983454d85f7d80f5e5f9d114d5
jsontype <jsontyper@gmail.com>
2022-09-10 14:19:58 +0900

    kimss

---
+++ front-js-3-reactjs/1-syntax(11)/4-syntax(고급-고급문법)(미완-수업전코드정리)/4-app-todo(미완)/2-todo-state-context-api/app1/src/components/TodoHead.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TodoHeadBlock = styled.div`
+    padding-top: 48px;
+    padding-left: 32px;
+    padding-right: 32px;
+    padding-bottom: 24px;
+    border-bottom: 1px solid #e9ecef;
+    h1 {
+        margin: 0;
+        font-size: 36px;
+        color: #343a40;
+    }
+    .day {
+        margin-top: 4px;
+        color: #868e96;
+        font-size: 21px;
+    }
+    .tasks-left {
+        color: #20c997;
+        font-size: 18px;
+        margin-top: 40px;
+        font-weight: bold;
+    }
+`
+
+function TodoHead() {
+    return (
+        <TodoHeadBlock>
+            <h1>2019년 7월 10일</h1>
+            <div className="day">수요일</div>
+            <div className="tasks-left">할 일 2개 남음</div>
+        </TodoHeadBlock>
+    )
+}
+
+export default TodoHead
