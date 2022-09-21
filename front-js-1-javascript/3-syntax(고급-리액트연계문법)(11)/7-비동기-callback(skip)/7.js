f8d6ba2946bec181e99012406a235ccc868d3b6c
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2022-09-21 22:28:54 +0900

    async

---
+++ front-js-1-javascript/3-syntax(고급-리액트연계문법)(11)/7-비동기-callback(skip)/7.js
@@ -0,0 +1,36 @@
+/*
+    [콜백 함수]
+
+    1. 콜백 함수
+        다른 함수에서 호출되어 실행되는 함수를 말한다. 
+        함수를 만들때, parameter나 return값을 함수로 받아서 쓸 수도 있는데, 그 함수도 callback 함수이다. 
+        * 잘 알아두면 이후 리액트할 때 프롭으로 함수를 넘겨주는 부분을 이해할 때 편해진다.
+*/
+
+// 1. 동기 처리에 콜백을 사용
+function introduce (lastName, firstName, callback) { // callback 함수사용법 1: callback을 인자에 넣는다.
+	let fullName = lastName + firstName
+    callback(fullName) // callback 함수사용법 2: callback을 마지막에 놓는다.
+} 
+introduce("홍", "길동", (name) => {console.log(name)}) // callback 함수사용법 3: 콜백을 사용하는 함수를 호출할 때는 () => {} 이렇게 "★★함수형 업데이트"를 통해서 호출해야 한다.
+
+// 1. 비동기 처리(setTimeout)에 콜백을 사용
+function work (callback) { // callback 함수사용법 1: callback을 인자에 넣는다.
+    setTimeout(() => {
+        const start = Date.now()
+        for (let i = 0; i < 1000000000; i++) { }
+        const end = Date.now()
+        console.log('비동기 setTimeout 작업 완료: ', end - start + 'ms')
+        callback() // callback 함수사용법 2: callback을 마지막에 놓는다.
+    }, 1000)
+}
+work(() => {}) // callback 함수사용법 3: 콜백을 사용하는 함수를 호출할 때는 work()가 아닌 work(() => {}) 이렇게 "★★함수형 업데이트"를 통해서 호출해야 한다.
+
+/*
+    1. 콜백 함수가 필요한 이유 : 비동기 방식으로 작성된 함수를 동기처럼 처리하기 위해 콜백이 필요하다.
+
+    1. 콜백 지옥
+        그러나, 이를 자주 하다보니 많은 비동기함수를 순차적으로 실행하려하다보면, 때때로 콜백 지옥이 발생하여 개발자들이 많은 어려움을 겪었다.
+        예를 들어 work1(() => { work2 (() => { work3 (() => {} ... }).bind(this)}).bind(this)}).bind(this) ... 이런 식의 코딩이 되어버리게 되었다.
+        그래서 이 후, 이를 promise나 async/await를 써서 해결하게 된다.
+*/
