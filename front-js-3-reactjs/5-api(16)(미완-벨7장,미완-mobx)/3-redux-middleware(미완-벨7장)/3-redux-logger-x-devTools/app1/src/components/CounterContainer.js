39c81c5588a8a3f06f2f5158747ad1da548d6b7f
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2022-10-05 02:33:39 +0900

    redux-logger

---
+++ front-js-3-reactjs/5-api(16)(미완-벨7장,미완-mobx)/3-redux-middleware(미완-벨7장)/3-redux-logger-x-devTools/app1/src/components/CounterContainer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Counter from './Counter'
+import { useSelector, useDispatch } from 'react-redux'
+import { increase, decrease } from '../modules/counter'
+
+// Counter의 컨테이너 컴포넌트 만들기
+function CounterContainer () {
+    const number = useSelector(state => state.counter)
+    const dispatch = useDispatch()
+
+    const onIncrease = () => {
+        dispatch(increase())
+    }
+    const onDecrease = () => {
+        dispatch(decrease())
+    }
+
+    return (
+        <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+    )
+}
+
+export default CounterContainer
