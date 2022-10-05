dd09f29ac2734ce7d910d3e3038016626d7d2c4d
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2022-10-06 02:20:38 +0900

    redux-thunk

---
+++ front-js-3-reactjs/5-api(16)(미완-벨7장,미완-mobx)/3-redux-middleware(미완-벨7장)/5-/app1/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import CounterContainer from './components/CounterContainer'
+
+function App () {
+  return <CounterContainer />
+}
+
+export default App
---
+++ front-js-3-reactjs/5-api(16)(미완-벨7장,미완-mobx)/3-redux-middleware(미완-벨7장)/5-/app1/src/components/CounterContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Counter from './Counter'
+import { useSelector, useDispatch } from 'react-redux'
+// Thunk 함수명으로 임포트 변경
+import { increaseAsync, decreaseAsync } from '../modules/counter'
+
+// Counter의 컨테이너 컴포넌트 만들기
+function CounterContainer () {
+    const number = useSelector(state => state.counter)
+    const dispatch = useDispatch()
+
+    // counter 컴포넌트에 선언한 Thunk 함수 onIncrease 사용
+    const onIncrease = () => {
+        dispatch(increaseAsync())
+    }
+    // counter 컴포넌트에 선언한 Thunk 함수 onDecrease 사용
+    const onDecrease = () => {
+        dispatch(decreaseAsync())
+    }
+
+    return (
+        <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+    )
+}
+
+export default CounterContainer
