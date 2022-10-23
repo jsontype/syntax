1acd7358e82b3952339233fa4b8a5ca12c587cb7
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2022-10-23 14:57:15 +0900

    refactor

--- front-js-1-javascript/1-syntax(초급-기초문법)(5)/4-반복문/4.js
+++ front-js-1-javascript/1-syntax(초급-기초문법)(5)/4-반복문/4.js
@@ -11,46 +11,6 @@ for (let i = 0; i < a.length; i++) {
     }
 }
 
-let b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-// 문제1 : b에서 3의 배수인 것만(b[i] % 3 = 0) 출력하세요.
-for (let i = 0; i < b.length; i++){
-    if (b[i] % 3 === 0) {
-        console.log(b[i])
-    }
-}
-
-// 문제2 : b의 모든 값들을 세제곱(b[i] * b[i])한 값을 출력하세요.
-for (let i = 0; i < b.length; i++){
-    console.log(b[i] ** 3)
-}
-
-// 문제3 : 구구단을 완성하세요. (이중 for문)
-for (let i = 2; i < 10; i++) {
-    console.log(i, '단 시작>>>');
-    for (let j = 1; j < 10; j++) {
-        console.log(i, 'x', j, '=', i * j)
-    }
-}
-
-
-
-// 배열을 위한 반복문 for...of : 나중에는 내장함수를 써서 별로 필요는 없지만, 알아만 두자.
-const numbers = [10, 20, 30, 40, 50]
-for (let item of numbers) {
-    console.log(item)
-}
-
-// 객체를 위한 반복문 for...in : 나중에는 내장함수를 써서 별로 필요는 없지만, 알아만 두자.
-const doggy = {
-    name: '멍멍이',
-    sound: '멍멍',
-    age: 2
-}
-for (let key in doggy) {
-    console.log(`${key}: ${doggy[key]}`)
-}
-
 // break 와 continue : 반복문 안에서는 break와 continue를 통하여 반복문에서 벗어나거나, 그 다음 루프를 돌게끔 할 수 있다.
 for (let i = 0; i < 10; i++) {
     // continue : 다음 루프를 실행
@@ -62,29 +22,61 @@ for (let i = 0; i < 10; i++) {
     if (i === 5) break
 }
 
-// 문제4 : 1에서 15까지 나열된 배열을 만들고 각 숫자의 2승의 총합을 구하는 반복문을 만들어보라.
-const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-let sum = 0
-for (let i = 0; i < numbers1.length; i++) {
-    sum += numbers1[i]
-}
-console.log(sum)
-// 더 쉬운 방법은 내장함수를 이용하는 방법이 있다.
-// const result = sumOf(numbers)
-// console.log(result) // 결과는 같다.
+// // 배열을 위한 반복문 for...of : 나중에는 내장함수를 써서 별로 필요는 없지만, 알아만 두자.
+// const arr = [10, 20, 30, 40, 50]
+// for (let item of arr) {
+//     console.log(item)
+// }
 
-// 문제5 : numbers1에서 3의 배수인 것만(b[i] % 3 = 0) 출력하라.
-for (let i = 0; i < numbers1.length; i++) {
-    if (numbers1[i] % 3 === 0) {
-        console.log(numbers1[i])
+// // 객체를 위한 반복문 for...in : 나중에는 내장함수를 써서 별로 필요는 없지만, 알아만 두자.
+// const doggy = {
+//     name: '멍멍이',
+//     sound: '멍멍',
+//     age: 2
+// }
+// for (let key in doggy) {
+//     console.log(`${key}: ${doggy[key]}`)
+// }
+
+
+
+/* 문제풀이 1 */
+
+let b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// 문제1 : b의 모든 값들의 총합을 구하는 반복문을 만들어보라.
+let sum1 = 0
+for (let i = 0; i < b.length; i++) {
+    sum1 += b[i]
+}
+console.log('답1: ', sum1)
+
+// 문제2 : b의 모든 값들을 세제곱(b[i] * b[i])한 값을 출력하세요.
+console.log('답2: ')
+for (let i = 0; i < b.length; i++){
+    console.log(b[i] ** 3)
+}
+
+// 문제3 : b의 각 값들의 2승의 총합을 구하는 반복문을 만들어보라.
+let sum2 = 0
+for (let i = 0; i < b.length; i++) {
+    sum2 += ( b[i] * b[i] )
+}
+console.log('답3: ', sum2)
+
+// 문제7 (if문필요) : b에서 3의 배수인 것만(b[i] % 3 = 0) 출력하세요.
+console.log('답7: ')
+for (let i = 0; i < b.length; i++){
+    if (b[i] % 3 === 0) {
+        console.log(b[i])
     }
 }
 
-// 문제6 : numbers1에서 7보다 큰 숫자로만 이루어진 배열을 새로 만들어서 반환하라.
-const numbers2 = []
-for (let i = 0; i < numbers1.length; i++) {
-    if (numbers1[i] > 7) {
-        numbers2.push(numbers1[i])
+// 문제9 (if문필요) : 구구단을 완성하세요. (이중 for문)
+console.log('답9: ')
+for (let i = 2; i < 10; i++) {
+    console.log(i, '단 시작>>>');
+    for (let j = 1; j < 10; j++) {
+        console.log(i, 'x', j, '=', i * j)
     }
 }
-console.log(numbers2)
--- front-js-3-reactjs/7-typescript(23)(미완-벨8장)/1-syntax(초급-기초문법)/8-ts-generics/app1/practice.ts
+++ front-js-3-reactjs/7-typescript(23)(미완-벨8장)/1-syntax(초급-기초문법)/8-ts-generics/app1/practice.ts
@@ -1,76 +1,41 @@
 // 1. 함수에서 Generics 사용하기
-// 예를 들어, 객체A와 객체B를 합쳐주는 merge라는 함수를 만든다고 가정해보자.
-// 그런 상황에서는 A와 B가 어떤 타입이 올 지 모르기 떄문에, 이런 상황에서는 아래와 같이 any라는 타입을 쓸 수도 있지만,
-// 결과가 any라는 것은 즉 merged 안에 무엇이 있는지 알 수 없다는 것이기 때문에, 타입 유추가 모두 깨진거나 다름이 없다.
-// 이런 상황에선, 주석 처리된 첫번째 merge 대신, 두번째 merge처럼 Generics를 사용하면 된다.
+// 어떤 타입이 올지 모르는 함수를 만든다면 타입 정의를 할 때 any를 쓸 수도 있지만,
+// 결과가 any라는 것은 안에 무엇이 있는지 알 수 없다는 것이기 때문에, 타입 유추가 모두 깨진거나 다름이 없다.
+// 이런 상황에선, any 대신에 Generics를 사용하면 된다.
 
-// function merge(a: any, b: any): any {
-//     return {
-//         ...a,
-//         ...b
-//     }
-// }
-// const merged = merge({ foo: 1 }, { bar: 1 })
-
-function merge<A, B>(a: A, b: B): A & B {
+// 함수에서 any를 사용 : 타입 유추가 모두 깨지며 타입 지원을 받을 수 없다.
+function wrap1(param: any):any {
     return {
-        ...a,
-        ...b
+        param
     }
 }
-const merged = merge({ foo: 1 }, { bar: 1 })
 
-// Generic 다른 예 : 함수에서 Generics를 사용하면 파라미터로 다양한 타입을 넣을 수도 있고 타입 지원을 지켜낼 수 있다.
-function wrap<T>(param: T) {
-  return {
-    param
-  }
+// 함수에서 Generics인 <T>를 사용 : 파라미터로 다양한 타입을 넣을 수도 있고 타입 지원을 지켜낼 수 있다.
+function wrap2<T>(param: T) { // 파라미터 () 뒤에 :any 쓰는 대신, 함수명 뒤에 <T>를 설정하고, T 타입으로 된 파라미터타입을 내부에서 설정해준다.
+    return {
+        param
+    }
 }
-const wrapped = wrap(10)
+const wrapped = wrap2(10) // wrap1처럼 아무 파라미터나 넣을 수 있다.
+
+
 
 // 1. interface에서 Generics 사용하기
-interface Items1<T> {
-  list: T[]
+interface Items1<T> { // 인터페이스명 옆에 :any 대신 <T>을 쓰고,
+    list: T[] // T 타입으로 된 배열을 내부에서 설정해준다.
 }
 
-const items1: Items1<string> = {
-  list: ['a', 'b', 'c']
-}
-
-
-// 1. type에서 Generics 사용하기
-type Items2<T> = {
-  list: T[]
-}
-
-const items2: Items2<string> = {
-  list: ['a', 'b', 'c']
+const items1: Items1<string> = { // string 배열을 넣을 거라면 :string 대신 :type명<string>을 쓰고,
+    list: ['a', 'b', 'c'] // string 타입으로 된 배열을 내부에서 설정해준다.
 }
 
 
-// 1. 클래스에서 Generics 사용하기
-class Queue<T> {
-  list: T[] = []
-  get length() {
-    return this.list.length
-  }
-  enqueue(item: T) {
-    this.list.push(item)
-  }
-  dequeue() {
-    return this.list.shift()
-  }
+
+// 1. ★ type에서 Generics 사용하기
+type Items2<T> = { // 타입명 옆에 :any 대신 <T>을 쓰고,
+  list: T[] // T 타입으로 된 배열을 내부에서 설정해준다.
 }
 
-// 1. 
-const queue = new Queue<number>()
-queue.enqueue(0)
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-queue.enqueue(4)
-console.log(queue.dequeue())
-console.log(queue.dequeue())
-console.log(queue.dequeue())
-console.log(queue.dequeue())
-console.log(queue.dequeue())
+const items2: Items2<string> = {// string 배열을 넣을 거라면 :string 대신 :type명<string>을 쓰고,
+  list: ['a', 'b', 'c'] // string 타입으로 된 배열을 내부에서 설정해준다.
+}
