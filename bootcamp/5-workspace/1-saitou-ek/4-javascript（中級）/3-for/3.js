93af20a56f7eeff87c5475289b5d6cfc04450d92
jsontype <jsontyper@gmail.com>
2022-10-30 01:15:22 +0900

    saitou

--- bootcamp/5-workspace/1-saitou-ek/4-javascript（中級）/3-for/3.js
+++ bootcamp/5-workspace/1-saitou-ek/4-javascript（中級）/3-for/3.js
@@ -8,10 +8,11 @@ const a = [1, 2, 3, 4, 5]
 a.forEach((item) => { console.log(item) })
 
 // map = 同じ長さの配列をreturnする配列高次関数
-// const b = a.map((item) => { return item + 1 })
+// const b = a.map((item) => { return item + 1 }) // 下と同じ
 const b = a.map(item => item + 1)
 console.log(b)
 
 // filter = filteringされてる配列をreturnする配列高次関数 (returnに条件付ける)
-const c = a.filter((item) => { return item > 2 })
+// const c = a.filter((item) => { return item > 2 }) // 下と同じ
+const c = a.filter(item => item > 2)
 console.log(c)
