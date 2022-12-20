777221b74bcbdb5bff012a3badf28a19a871338e
jsontype <jsontyper@gmail.com>
2022-12-20 14:12:27 +0900

    refactor

--- front-7-vue/1-vue/1-vue2/1-vue2-movie/2-vuex-read(movie)/app1/src/App.vue
+++ front-7-vue/1-vue/1-vue2/1-vue2-movie/2-vuex-read(movie)/app1/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="app">
-    <HelloWorld msg="Hello" />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -16,7 +17,12 @@ export default {
 </script>
 
 <style>
-.app {
-  margin: 20px;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
