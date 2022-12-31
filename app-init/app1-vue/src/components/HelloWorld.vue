bc6a5410d26e9e10cfe4829181336d00df514a57
jsontype <jsontyper@gmail.com>
2022-12-31 23:53:12 +0900

    refactor

---
+++ app-init/app1-vue/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.hello {
+  color: red;
+}
+</style>
