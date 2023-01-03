625792108b23afe6fe9dce1e8b42bd5efb5e31b1
jsontype <jsontyper@gmail.com>
2023-01-03 23:56:24 +0900

    vue와 react 절차 동일하게

---
+++ front-7-vue/1-vue/1-vue2/2-vue2(초급-기초)/2-components(미완)/app1/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    Hello {{ name }}!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: 'yang',
+    }
+  }
+}
+</script>
+
+<style>
+.app {
+  margin: 20px;
+}
+</style>
