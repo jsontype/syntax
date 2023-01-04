33af57ec490be580d1573ca2e6e9620c87ce043d
jsontype <jsontyper@gmail.com>
2023-01-04 20:39:26 +0900

    vue 4 prepare

---
+++ front-7-vue/1-vue(미완)/1-vue2(미완)/2-vue2(초급-기초)(미완)/4-props-children(미완)/app1/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <Hello />
+    <Hello />
+    <Hello />
+    <!-- 1. 프롭 준다. ★ Template 문법상, 프롭을 변수로 줄때는 :을 사용한다. -->
+    <Hello :name="name" mark="!" />
+  </div>
+</template>
+
+<script>
+import Hello from './Hello'
+
+export default {
+  name: 'App',
+  components: {
+    Hello,
+  },
+  data() {
+    return {
+      name: 'Yang',
+    }
+  }
+}
+</script>
+
+<style>
+.hello {
+  font-size: 22px;
+}
+</style>
