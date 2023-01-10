6662133cd401d0cf1426cfc88e9045c7db311cd6
jsontype <jsontyper@gmail.com>
2023-01-10 20:31:26 +0900

    refactor

---
+++ backend/back-2-nodejs(3)/3-node-express-server(미완)/2-api-back과연결/2-db-mysql(미완)/2-mysql-x-express(미완)/app1/index.js
@@ -0,0 +1,16 @@
+// index.js
+const express = require("express")
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+  return res.send("Hello World!")
+})
+
+const userRouter = require("./routes/users")
+app.use("/users", userRouter)
+
+app.listen(port, function () {
+  console.log(`Server listening on port ${port}`)
+})
