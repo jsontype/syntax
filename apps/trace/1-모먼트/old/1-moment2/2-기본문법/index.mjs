d023d0cd0902772141b46963b20b3f2466ac010e
jsontype <jsontyper@gmail.com>
2023-01-26 01:03:13 +0900

    infra

--- apps/trace/1-모먼트/old/1-moment2/2-기본문법/index.mjs
+++
@@ -1,7 +0,0 @@
-// var moment = require("moment") // require
-
-// console.log(moment()); // 지금시간
-
-import moment from "moment"
-
-console.log(moment())
--- apps/trace/1-모먼트/old/2-dateFns/1.js
+++
@@ -1,3 +0,0 @@
-import { formatDistance, subDays } from "date-fns"
-
-formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
