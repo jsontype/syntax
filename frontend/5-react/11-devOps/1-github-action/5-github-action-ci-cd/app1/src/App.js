a3c86d6c989e2906d2a1a0d72b295a554dd830c3
jsontype <jsontyper@gmail.com>
2023-04-12 20:09:48 +0900

    ci

--- frontend/5-react/11-devOps/1-github-action/5-github-action-ci-cd/app1/src/App.js
+++ frontend/5-react/11-devOps/1-github-action/5-github-action-ci-cd/app1/src/App.js
@@ -1,9 +1,25 @@
-import "./App.css"
+// 이 부분은 수동으로 수정해야한다. 수정후 아래 XML 태그쪽에 빨간 줄이 없어질 것이다.
+// 아직 ESLint가 모든 것을 수정해주지는 않는다.
+import React from 'react'
+import './App.css'
 
 export default function App() {
+  const onClick = text => {
+    console.log(text)
+  }
+
   return (
+    // 이 부분은 npm run lint:fix로 자동 수정이 가능할 것이다. 
+    // ESLint가 자동수정 가능한 항목은 https://eslint.org/docs/latest/rules/에서 확인할 수 있다.
     <div className="App">
-      <div>Hello React!</div>
+      <div
+        onClick={() => {
+          onClick("Hello React!")
+        }}
+        style={{ color: 'gray' }}
+      >
+        Hello React!
+      </div>
     </div>
   )
 }
