35d4e6af1acda22d5bb046f29d700c7316d3d82b
jsontype <jsontyper@gmail.com>
2023-05-01 23:31:17 +0900

    refactor

--- frontend/6-react/2-next/1-next/3-next-back/1-database/app1/src/app/page.js
+++ frontend/6-react/2-next/1-next/3-next-back/1-database/app1/src/app/page.js
@@ -1,6 +1,14 @@
 import styles from "./page.module.css"
+import { connectDB } from "../../util/database"
 
-export default function Home() {
+export default async function Home() {
+  // db명 : "db1"
+  const db = (await connectDB).db("db1")
+  // collection명 : "bbs1"
+  const result = await db.collection("bbs1").find().toArray()
+  // db 호출 결과 표시
+  console.log(result)
+
   return (
     <>
       <div className={styles.red}>Hello Next</div>
