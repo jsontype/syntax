46468829c22ba5192ab0f24dd319cb7333e647d8
jsontype <jsontyper@gmail.com>
2023-05-03 08:39:28 +0900

    comment

--- frontend/6-react/2-next/1-next/3-next-back/6-bbs-edit/app1/src/pages/api/bbs/write.js
+++ frontend/6-react/2-next/1-next/3-next-back/6-bbs-edit/app1/src/pages/api/bbs/write.js
@@ -2,7 +2,14 @@ import { connectDB } from "@/app/api/connectDB"
 
 export default async function handler(req, res) {
   if (req.method == "POST") {
-    console.log(req.body)
+    /*
+      console.log(req.body) // req.body는 현재 새 title, 새 content가 들어있다.
+      console.log(req.query) // req.query는 현재 없다. 
+        만일 주소창에 ?name=yang&age=20 같은 쿼리가 있다면, 
+          console.log(req.query) 안에, { name: 'yang', age: '20' } 으로 담겨져서 쓸 수 있다.
+        만일 api/delete/[name].js를 만든뒤, fetch('/api/delete/yang')를 하게 되면, 
+          console.log(req.query) 안에, { name: 'yang' } 으로 담겨져서 쓸 수 있다.
+    */
     if (req.body.title == "") {
       return res.status(500).json("제목은 필수입니다.")
     }
