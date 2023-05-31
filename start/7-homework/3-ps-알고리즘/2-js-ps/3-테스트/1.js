5f8c6ce315d9ae980aaca37777993c6577d3821a
jsontype <jsontyper@gmail.com>
2023-05-31 14:32:10 +0900

    vue mutation apollo

--- start/7-homework/3-ps-알고리즘/2-js-ps/3-테스트/1.js
+++ start/7-homework/3-ps-알고리즘/2-js-ps/3-테스트/1.js
@@ -1,8 +1,14 @@
 // 입력
-const input = [1000, 100]
+const input = `1
+-1`
 
 // 출력
-const a = Number(input[0])
-const b = Number(input[1])
-console.log(BigInt(Math.floor(a / b)))
-console.log(BigInt(Math.floor(a % b)))
+const inputSplit = input.split(`
+`)
+const a = Number(inputSplit[0])
+const b = Number(inputSplit[1])
+console.log(a)
+console.log(b)
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
