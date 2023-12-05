4fcf8824658a419df001da14b03c8eaac7e26470
jsontype <jsontyper@gmail.com>
2023-12-06 00:02:24 +0900

    mzk

---
+++ frontend/5-vue/2-vue3/1-vue2-vs-vue3/2-vue3-count-app/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import CountApp from './components/CountApp.vue'
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+function onClick() {
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+  <button @click="onClick">Toggle</button>
+  <div v-if="isOpen">
+    <CountApp />
+  </div>
+</template>
