b66423c4624b6b4359afd1ab446563405afd1bb2
jsontype <jsontyper@gmail.com>
2024-01-06 17:04:13 +0900

    lee

---
+++ frontend/5-vue/2-vue3/1-syntax(미완)/2-props/app1/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <HelloWorld msg1="Hello" msg2="Vue3" :on-click="onClick" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+function onClick() {
+  console.log('clicked')
+}
+</script>
+
+<style scoped>
+</style>
