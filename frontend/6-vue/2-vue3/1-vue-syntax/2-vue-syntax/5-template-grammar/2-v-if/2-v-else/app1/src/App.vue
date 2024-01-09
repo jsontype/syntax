25b8f66f35bee0f9508be4c27ede35c0b6fda53a
jsontype <jsontyper@gmail.com>
2024-01-10 01:06:20 +0900

    mzk

---
+++ frontend/6-vue/2-vue3/1-vue-syntax/2-vue-syntax/5-template-grammar/2-v-if/2-v-else/app1/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <HelloWorld msg="Hello" />
+  <GlobalHelloWorld msg="Vue3" />
+  <hr />
+  <VBind />
+  <VBindProps 
+    :string1="str1" 
+    :string2="str2" 
+    :number1="num1" 
+    :boolean1="bool1"
+    :on-click="showAlert"
+  />
+  <hr />
+  <VIf />
+  <VElse />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue'
+import VBind from '@/components/VBind.vue'
+import VBindProps from '@/components/VBindProps.vue'
+import VIf from '@/components/VIf.vue'
+import VElse from '@/components/VElse.vue'
+
+const str1 = 'String 1'
+const str2 = 'String 2'
+const num1 = 9999
+const bool1 = num1 > 9998
+const showAlert = () => {
+  alert('clicked')
+}
+</script>
+
+<style scoped>
+</style>
