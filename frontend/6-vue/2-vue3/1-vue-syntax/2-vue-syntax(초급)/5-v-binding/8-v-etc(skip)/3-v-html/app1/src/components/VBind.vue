b053aec3321ba3987e3d4a76f3bceace6684eb28
jsontype <jsontyper@gmail.com>
2024-01-15 15:20:10 +0900

    v-binding 90%

---
+++ frontend/6-vue/2-vue3/1-vue-syntax/2-vue-syntax(초급)/5-v-binding/8-v-etc(skip)/3-v-html/app1/src/components/VBind.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- 데이터 바인딩 -->
+    <img v-bind:src="imageURL" alt="Vue Logo" />
+    <!-- 축약형 -->
+    <img :src="imageURL" alt="Vue Logo" />
+  </div>
+</template>
+
+<script setup>
+const imageURL = 'https://vuejs.org/images/logo.png'
+</script>
