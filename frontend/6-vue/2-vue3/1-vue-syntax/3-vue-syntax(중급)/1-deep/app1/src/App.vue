50e38ed31db07bf3134001d4bd85a315ad247050
jsontype <jsontyper@gmail.com>
2024-01-18 01:16:58 +0900

    vue3 into the intermd

---
+++ frontend/6-vue/2-vue3/1-vue-syntax/3-vue-syntax(중급)/1-deep/app1/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <NoDeep />
+  <Deep />
+</template>
+
+<script setup lang="ts">
+import NoDeep from '@/components/NoDeep.vue'
+import Deep from '@/components/Deep.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/6-vue/2-vue3/1-vue-syntax/3-vue-syntax(중급)/1-deep/app1/src/components/Deep.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>User Profile</h2>
+    <input v-model="user.userName" placeholder="Name" />
+    <input v-model="user.details.age" placeholder="Age" />
+    <div>Updated at: {{ lastUpdated }}</div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref, watch } from 'vue';
+
+// 사용자 정보를 reactive 객체로 정의
+const user = reactive({
+  userName: '',
+  details: {
+    age: ''
+  }
+});
+
+const lastUpdated = ref('');
+
+// user의 하위 status들의 변경을 감시하려면 deep 옵션이 필요함
+watch(() => user, (newValue, oldValue) => {
+  lastUpdated.value = new Date().toLocaleTimeString();
+}, { deep: true });
+</script>
