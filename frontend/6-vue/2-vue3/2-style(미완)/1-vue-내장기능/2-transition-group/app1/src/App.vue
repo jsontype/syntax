0dbff9d79b2630743f15bf2ebb1e8eab81d473dd
jsontype <jsontyper@gmail.com>
2024-01-23 23:26:36 +0900

    vue3 bootstrap

---
+++ frontend/6-vue/2-vue3/2-style(미완)/1-vue-내장기능/2-transition-group/app1/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <ListTransition />
+</template>
+
+<script setup lang="ts">
+import ListTransition from '@/components/ListTransition.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/6-vue/2-vue3/2-style(미완)/1-vue-내장기능/2-transition-group/app1/src/components/ListTransition.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Transition Group: List의 Transition 처리</h1>
+  <button @click="addItem">Add Item</button>
+  <transition-group name="list">
+    <div v-for="item in items" :key="item">
+      {{ item }}
+    </div>
+  </transition-group>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const items = ref(['Item 1', 'Item 2', 'Item 3'])
+const addItem = () => {
+  const nextItem = `Item ${items.value.length + 1}`
+  items.value.push(nextItem)
+}
+</script>
+
+<style scoped>
+.list-enter-active, .list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from, .list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
