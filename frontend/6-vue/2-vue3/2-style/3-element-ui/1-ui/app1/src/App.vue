cea842cdf74a1a37e58bf48e259362341720caaf
jsontype <jsontyper@gmail.com>
2024-01-24 15:57:14 +0900

    vue style

---
+++ frontend/6-vue/2-vue3/2-style/3-element-ui/1-ui/app1/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <ElButtonExample />
+  <ElInputExample />
+</template>
+
+<script setup lang="ts">
+import ElButtonExample from '@/components/ElButtonExample.vue'
+import ElInputExample from '@/components/ElInputExample.vue'
+</script>
+
+<style scoped>
+</style>
