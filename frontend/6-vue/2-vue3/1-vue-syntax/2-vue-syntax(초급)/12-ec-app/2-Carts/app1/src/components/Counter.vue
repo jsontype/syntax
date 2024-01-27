147b6ec9586b05807297474d540fc308cb07662d
jsontype <jsontyper@gmail.com>
2024-01-27 14:02:59 +0900

    lee

---
+++ frontend/6-vue/2-vue3/1-vue-syntax/2-vue-syntax(초급)/12-ec-app/2-Carts/app1/src/components/Counter.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="countBtns">
+      <button class="counterBtn" @click="add(props.price)">+</button>
+      <button class="counterBtn" @click="minus(props.price)">-</button>
+    </div>
+    <div>수량: {{ count }}</div>
+    <div>예상 가격: $ {{ predictedPrice }}</div>
+  </div>
+
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue'
+
+const count = ref(0)
+const predictedPrice = ref(0)
+
+const emit = defineEmits(['changeCount'])
+
+const props = defineProps({
+  price: Number
+})
+
+function add (price) {
+  count.value++
+  predictedPrice.value = props.price * count.value
+  emit("changeCount", "+")
+}
+
+function minus (price) {
+  count.value > 0 && count.value--
+  predictedPrice.value = props.price * count.value
+  emit("changeCount", "-")
+}
+</script>
+
+<style scoped>
+.countBtns {
+  margin-bottom: 10px
+}
+
+.counterBtn {
+  width: 30px;
+}
+</style>
