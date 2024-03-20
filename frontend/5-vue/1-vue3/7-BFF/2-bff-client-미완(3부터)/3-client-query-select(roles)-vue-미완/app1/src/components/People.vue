cd224740e02ceddfb9a41b5ab3d77417a5507180
jsontype <jsontyper@gmail.com>
2024-03-20 20:15:39 +0900

    study

---
+++ frontend/5-vue/1-vue3/7-BFF/2-bff-client-미완(3부터)/3-client-query-select(roles)-vue-미완/app1/src/components/People.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="people" class="component">
+    <aside>
+      <div></div>
+    </aside>
+    <section class="contents">
+      <div></div>
+    </section>
+  </div>
+</template>
