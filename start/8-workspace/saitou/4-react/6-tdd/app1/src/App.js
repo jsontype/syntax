15d4ebdb2ea2cd1ae56371ed2ec85d6585e6dadf
jsontype <jsontyper@gmail.com>
2024-07-15 19:04:08 +0900

    fix: eslint

---
+++ start/8-workspace/saitou/4-react/6-tdd/app1/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Profile from './Profile'
+
+export default function App () {
+  return (
+    <div className="app">
+      <Profile username="Jsontyper" name="D.Yang" />
+    </div>
+  )
+}
