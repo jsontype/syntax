0c8e1bb416b1064485e5bd144a875a23a5b817b9
jsontype <jsontyper@gmail.com>
2024-07-22 22:54:43 +0900

    docker

---
+++ ops/6-docker/2-dockerfile/app1/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
