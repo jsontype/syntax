043c767a6d49b0f5f4b37d056f0ba95f99bdbfac
jsontype <jsontyper@gmail.com>
2024-07-28 18:16:47 +0900

    nuxt3 가닥 잡기

---
+++ frontend/6-vue/2-nuxt3/2-nuxt3-client/2-routing/app1/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2 class="title">홈 화면</h2>
+  </div>
+</template>
+
+<style>
+.title {
+  color: gray;
+}
+</style>
---
+++ frontend/6-vue/2-nuxt3/2-nuxt3-client/2-routing/app1/pages/list.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="title">상품목록</h2>
+    <div>
+      <div class="item">상품1 $40</div>
+      <div class="item">상품2 $40</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.title {
+  color: gray;
+}
+
+.item {
+  color: gray;
+  font-size: 14px;
+}
+
+.item:hover {
+  color: pink;
+}
+</style>
