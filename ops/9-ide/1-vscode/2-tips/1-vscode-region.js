398a13c109d38481afb62e61fe51524c247e17d2
jsontype <jsontyper@gmail.com>
2024-08-31 15:18:36 +0900

    feat: tips for vscode

---
+++ ops/9-ide/1-vscode/2-tips/1-vscode-region.js
@@ -0,0 +1,42 @@
+// #region 설명
+const explainRegion = "js는, #region 이나 region 주석을 사용해서 코드의 위치를 지정해두면, 긴 코드의 경우, 우측의 사이드바를 통해 위치보기 및 이동이 가능하므로 보기 편리하다."
+const explainEndregion = "js는, #endregion 이나 endregion 주석을 사용해서 레전을 닫을 수 있다."
+console.log(explainRegion);
+console.log(explainRegion);
+console.log(explainRegion);
+console.log(explainRegion);
+console.log(explainRegion);
+console.log(explainEndregion);
+console.log(explainEndregion);
+console.log(explainEndregion);
+console.log(explainEndregion);
+console.log(explainEndregion);
+// #endregion 설명
+
+// region Define A
+const a = 1;
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+// endregion Define A
+
+// region Define B
+const b = 2;
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+// endregion Define B
