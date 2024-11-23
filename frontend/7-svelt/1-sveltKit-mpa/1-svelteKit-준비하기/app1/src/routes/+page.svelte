07cb990a545aa0f7bb0d42fffe9730663db1de66
jsontype <jsontyper@gmail.com>
2024-11-23 19:17:06 +0900

    svelt

---
+++ frontend/7-svelt/1-sveltKit-mpa/1-svelteKit-준비하기/app1/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Hello Svelte!</h1>
+
+<script>
+// vite.config.js에서 src를 절대경로 설정해두었으므로, import는 아래처럼 절대경로를 사용할 것
+// import ...Component from 'src/components/.../index.svelte';
+</script>
+
+<style>
+</style>
