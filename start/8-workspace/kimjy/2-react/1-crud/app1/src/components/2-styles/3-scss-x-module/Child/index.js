d7548f167a48d5f79b70af144e3a282f0f205cd8
jsontype <jsontyper@gmail.com>
2025-04-05 22:08:29 +0900

    refactor

--- start/8-workspace/kimjy/2-react/1-crud/app1/src/components/2-styles/3-scss-x-module/Child/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "./style.module.scss"
-
-export default function index() {
-  return (
-    <div>
-      <h3 className={styles.app}>CssModule Child</h3>
-    </div>
-  )
-}
--- start/8-workspace/kimjy/2-react/2-styles/2-tailwindcss/app1/src/components/MuiButton/index.js
+++
@@ -1,7 +0,0 @@
-import Button from "@mui/material/Button"
-
-export default function MuiButton({ variant = "contained", type = "primary", text }) {
-  return (
-    <Button variant={variant} color={type}>{text}</Button>
-  )
-}
