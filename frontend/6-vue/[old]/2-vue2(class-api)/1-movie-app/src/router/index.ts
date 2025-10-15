f2d9d81dba0815e2ec30c0a74cebb6feb0fbc287
jsontype <jsontyper@gmail.com>
2025-10-15 19:16:28 +0900

    refactor

---
+++ frontend/6-vue/[old]/2-vue2(class-api)/1-movie-app/src/router/index.ts
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/HomeView.vue"),
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/movies",
+    name: "movies",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/MoviesView.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
